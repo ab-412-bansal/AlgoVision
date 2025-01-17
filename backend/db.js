39b2b941cc38a7b5aab56bc7b9c4cbfe3b0b0dd8
Ayush Bansal <139624384+ab-412-bansal@users.noreply.github.com>
2025-01-17 23:33:17 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectToDb(){
     try {
-        await mongoose.connect("mongodb+srv://ayush4bansal:gzGQsyn6MsWT0GdF@cluster0.8ybir.mongodb.net/algovision_db")
+        await mongoose.connect("your_mongoode_database_link")
         console.log("Connected to db")
     } catch (error) {
         console.log("Error: ", error)        
